11a6bfbed405ae03888be7ba1ec7fb874f2c26ec
w1nkl34 <sevan98t@gmail.com>
2022-06-08 19:43:00 +0300

    nn

--- Assets/Main/Scripts/Constants.cs
+++ Assets/Main/Scripts/Constants.cs
@@ -6,9 +6,9 @@ public static class Constants
 {
     public static int spawnPool = 0;
     public static int hitParticleCount = 0;
-    public static int maxHitParticleCount = 15;
+    public static int maxHitParticleCount = 10;
     public static int damageTextCount = 0;
-    public static int maxDamageTextCount = 15;
+    public static int maxDamageTextCount = 10;
 
 }
 
